5e746d7cb96d2bdd60c892334e9294ad91faf13e
walingar <Walingar@mail.ru>
2018-07-20 16:49:50 +0300

    Added lazy

--- log-events/src/com/jetbrains/gitalso/json/GitAlsoJsonSerializer.kt
+++ log-events/src/com/jetbrains/gitalso/json/GitAlsoJsonSerializer.kt
@@ -8,8 +8,10 @@ import java.math.BigDecimal
 object GitAlsoJsonSerializer {
 
     // Gson which prints double numbers with 2 digits after .
-    private val gson = GsonBuilder().serializeNulls().registerTypeAdapter(object: TypeToken<Double>() {}.type, JsonSerializer<Double>
-    { value, _, _ -> JsonPrimitive(BigDecimal(value.toDouble()).setScale(2, BigDecimal.ROUND_HALF_UP)) }).create()
+    private val gson by lazy {
+        GsonBuilder().serializeNulls().registerTypeAdapter(object : TypeToken<Double>() {}.type, JsonSerializer<Double>
+        { value, _, _ -> JsonPrimitive(BigDecimal(value.toDouble()).setScale(2, BigDecimal.ROUND_HALF_UP)) }).create()
+    }
 
     fun toJson(obj: Any): String = gson.toJson(obj)
 
