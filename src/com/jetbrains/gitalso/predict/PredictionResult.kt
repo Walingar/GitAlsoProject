171d3bcd2e5e8e4daf67d6c3806252f7c714df91
walingar <Walingar@mail.ru>
2018-07-31 15:15:01 +0300

    Update for logging

--- src/com/jetbrains/gitalso/predict/PredictionResult.kt
+++ src/com/jetbrains/gitalso/predict/PredictionResult.kt
@@ -6,24 +6,44 @@ import com.jetbrains.gitalso.log.Action
 import com.jetbrains.gitalso.log.Factor
 import com.jetbrains.gitalso.log.LogEvent
 import com.jetbrains.gitalso.log.LogField
+import com.jetbrains.gitalso.log.State
 
 data class PredictionResult(val scores: Map<Pair<CommittedFile, CommittedFile>, Number>, val prediction: Collection<CommittedFile>) {
-    fun getLogEvent(repository: String, action: Action): LogEvent {
-        val timestamp = System.currentTimeMillis()
-        val recorderID = "git-also"
-        val recorderVersion = "1"
-        val userID = PermanentInstallationID.get()
-        val sessionID = "1"
-        val bucket = "-1"
+    private val timestamp by lazy {
+        System.currentTimeMillis()
+    }
+    private val recorderID = "git-also"
+    private val recorderVersion = "1"
+    private val userID by lazy {
+        PermanentInstallationID.get()
+    }
+    private val bucket = "-1"
+    var repository = "null"
+    var sessionID = 1
+
+    fun getLogEvent(
+            stateBefore: State,
+            stateAfter: State,
+            action: Action,
+            commits: Map<Pair<CommittedFile, CommittedFile>, Set<Long>>
+    ): LogEvent {
         val factors = HashMap<String, Map<Factor, Any>>()
         for ((key, value) in scores) {
-            factors[key.toString()] = mapOf(Factor.SCORES to value)
+            if (key !in commits) {
+                continue
+            }
+            factors[key.toString()] = mapOf(
+                    Factor.SCORES to value,
+                    Factor.COMMITS to commits[key]!!)
         }
+
         val fields = mapOf(
+                LogField.STATE_BEFORE to stateBefore,
+                LogField.STATE_AFTER to stateAfter,
                 LogField.REPOSITORY to repository,
                 LogField.FACTORS to factors
         )
 
-        return LogEvent(timestamp, recorderID, recorderVersion, userID, sessionID, action, bucket, fields)
+        return LogEvent(timestamp, recorderID, recorderVersion, userID, sessionID.toString(), action, bucket, fields)
     }
 }
