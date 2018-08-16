cea2bffb1cf6dff9e20d3167a4c4b01558612dbb
walingar <Walingar@mail.ru>
2018-08-16 13:17:48 +0300

    Added toolbar with Collapse and Expand all

--- src/com/jetbrains/gitalso/commitHandle/ui/PredictionDialog.kt
+++ src/com/jetbrains/gitalso/commitHandle/ui/PredictionDialog.kt
@@ -1,13 +1,17 @@
 package com.jetbrains.gitalso.commitHandle.ui
 
+
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.ScrollPaneFactory
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Toolkit
+import com.intellij.ui.treeStructure.Tree
+import com.intellij.ui.treeStructure.actions.CollapseAllAction
+import com.intellij.ui.treeStructure.actions.ExpandAllAction
+import java.awt.*
 import javax.swing.*
 
 class PredictionDialog(project: Project, private val root: VirtualFile, private val drawable: Set<VirtualFile>, private val color: Color) : DialogWrapper(project) {
@@ -20,16 +24,32 @@ class PredictionDialog(project: Project, private val root: VirtualFile, private 
         return arrayOf(DialogWrapperExitAction("Close", 0))
     }
 
+    private fun createActionsPanel(predictionTreeChange: Tree): JPanel {
+        val panel = JPanel(BorderLayout())
+
+        val actionGroup = DefaultActionGroup()
+        actionGroup.add(ExpandAllAction(predictionTreeChange))
+        actionGroup.add(CollapseAllAction(predictionTreeChange))
+
+        val toolbar = ActionManager.getInstance().createActionToolbar("GitAlso.PredictionDialog", actionGroup, true).component
+        panel.add(toolbar, BorderLayout.LINE_END)
+        return panel
+    }
+
     override fun createCenterPanel(): JComponent? {
         val panel = JPanel(BorderLayout())
-        val predictionTreeChange = PredictionTree(root, drawable, color)
-        val scrollPane = ScrollPaneFactory.createScrollPane(predictionTreeChange.getTree())
+        val predictionTreeChange = PredictionTree(root, drawable, color).getTree()
+        panel.add(createActionsPanel(predictionTreeChange), BorderLayout.PAGE_START)
+        val scrollPane = ScrollPaneFactory.createScrollPane(predictionTreeChange)
+
+        val screenSize = Toolkit.getDefaultToolkit().screenSize
 
         scrollPane.preferredSize = Dimension(
-                50 + scrollPane.preferredSize.width,
-                50 * predictionTreeChange.getTree().rowCount)
+                (screenSize.width * 0.2).toInt(),
+                (screenSize.height * 0.3).toInt())
 
-        panel.add(scrollPane)
+
+        panel.add(scrollPane, BorderLayout.CENTER)
 
         return panel
     }
