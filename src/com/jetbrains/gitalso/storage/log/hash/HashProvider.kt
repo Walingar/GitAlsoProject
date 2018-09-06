79712c766c5322e9a08c1b829ddf62d82aeeb573
walingar <Walingar@mail.ru>
2018-09-06 13:45:00 +0300

    Updated hash counting

--- src/com/jetbrains/gitalso/storage/log/hash/HashProvider.kt
+++ src/com/jetbrains/gitalso/storage/log/hash/HashProvider.kt
@@ -1,14 +1,26 @@
 package com.jetbrains.gitalso.storage.log.hash
 
+import java.security.MessageDigest
+
 object HashProvider {
-    fun hash(st: String): Long {
+
+    private fun bytesToLong(bytes: ByteArray?): Long {
+        if (bytes == null) {
+            return 0
+        }
         val p = 31L
         var hash = 0L
         var pPow = 1L
-        for (ch in st) {
-            hash += (ch - 'a' + 1) * pPow
+        for (ch in bytes) {
+            hash += ch * pPow
             pPow *= p
         }
         return hash
+    }
+
+    fun hash(st: String): Long {
+        val md = MessageDigest.getInstance("SHA-256")
+        md.update(st.toByteArray())
+        return bytesToLong(md.digest())
     }
 }
