29c69b5c34271f4ab4fb25da8b618bc4a08c9c60
Walingar <Walingar@mail.ru>
2018-11-06 11:06:27 +0300

    Update UserInfo

--- src/com/jetbrains/gitalso/plugin/UserStorage.kt
+++ src/com/jetbrains/gitalso/plugin/UserStorage.kt
@@ -3,18 +3,17 @@ package com.jetbrains.gitalso.plugin
 import com.intellij.openapi.components.*
 import kotlin.math.max
 import kotlin.math.min
-import kotlin.math.pow
 
 @State(name = "GitAlsoUserStorage", storages = [Storage(file = "GitAlsoUserStorage.xml")])
 object UserStorage : PersistentStateComponent<UserStorage.State> {
     class State {
         var lastAction = "Commit"
-        var step = 0
-        var threshold = 0.3
-        var isTurnedOff = false
+        var step = 0.0
+        var threshold = 0.35
+        var isTurnedOn = true
     }
 
-    var currentState = State()
+    private var currentState = State()
 
     override fun loadState(state: State) {
         currentState = state
@@ -22,23 +21,27 @@ object UserStorage : PersistentStateComponent<UserStorage.State> {
 
     override fun getState() = currentState
 
-    fun userStorageUpdate(storage: UserStorage.State, type: String) {
-        if (storage.lastAction == type) {
-            storage.step++
-        } else {
-            storage.step = 0
+    fun UserStorage.State.userStorageUpdate(type: String) {
+        val storage = this
+        val gamma = 0.9
+
+        if (type == "Cancel" && step <= 0) {
+            threshold -= 0.005
+            storage.lastAction = type
+            return
         }
         storage.lastAction = type
-        when (type) {
+
+        storage.step *= gamma
+        storage.step = when (type) {
             "Commit" -> {
-                storage.threshold += 0.05 / (1 + 2.71.pow(-0.2 * storage.step))
-                storage.threshold = min(storage.threshold, 0.7)
+                min(((1 - storage.threshold) / 7), storage.step + 0.05)
             }
             "Cancel" -> {
-                storage.threshold -= 0.01 / (1 + 2.71.pow(-0.05 * storage.step))
-                storage.threshold = max(storage.threshold, 0.15)
+                max(-(storage.threshold / 7), storage.step - 0.05)
             }
-            else -> throw IllegalStateException()
+            else -> 0.0
         }
+        storage.threshold = min(max(storage.threshold + storage.step, 0.15), 1.0)
     }
 }
