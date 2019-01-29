f7c586d86a5e3065cfa12170d4d0af39900da504
Walingar <Walingar@mail.ru>
2019-01-29 21:57:34 +0300

    Removed redundant method

--- src/com/jetbrains/gitalso/commitHandle/util.kt
+++ src/com/jetbrains/gitalso/commitHandle/util.kt
@@ -3,9 +3,6 @@ package com.jetbrains.gitalso.commitHandle
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.vcs.log.VcsRef
 import com.intellij.vcs.log.data.RefsModel
-import java.util.*
-
-fun ClosedRange<Int>.random() = Random().nextInt((endInclusive + 1) - start) + start
 
 fun RefsModel.findBranch(root: VirtualFile, branchName: String): VcsRef? {
     val branches = this.allRefsByRoot[root]?.streamBranches() ?: return null
