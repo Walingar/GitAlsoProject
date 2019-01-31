235f370ca28549e7f976cf7bf3f5952b3f2f52ec
Walingar <Walingar@mail.ru>
2019-01-31 20:39:50 +0300

    Cleanup and back to previous UI creation

--- src/com/jetbrains/gitalso/commitHandle/ui/GitAlsoDialog.kt
+++ src/com/jetbrains/gitalso/commitHandle/ui/GitAlsoDialog.kt
@@ -5,11 +5,16 @@ import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.vcs.changes.Change.Type
 import com.intellij.openapi.vcs.changes.ui.ChangesTree
 import com.intellij.openapi.vcs.changes.ui.TreeActionsToolbarPanel
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.layout.panel
+import com.intellij.util.ui.JBDimension
+import com.jetbrains.gitalso.predict.PredictedChange
 import com.jetbrains.gitalso.predict.PredictedFile
+import java.awt.BorderLayout
 import javax.swing.Action
 import javax.swing.JPanel
 
@@ -46,24 +51,49 @@ class GitAlsoDialog(private val project: Project, private val files: List<Predic
         return TreeActionsToolbarPanel(toolbar, predictionTreeChange)
     }
 
-    private fun createTreePanel() = panel {
+    private fun createTreeScrollPane(predictionTreeChange: ChangesTree): JBScrollPane {
+        val scrollPane = JBScrollPane(predictionTreeChange)
+        scrollPane.preferredSize = JBDimension(400, 300)
+        return scrollPane
+    }
+
+    private fun createTreePanel(): JPanel {
         val predictionTreeChange = PredictedFilesTreeImpl(project, false, false, files)
         tree = predictionTreeChange
-        row {
-            cell(true) {
-                createActionsPanel(predictionTreeChange)(growX, growY)
-                scrollPane(predictionTreeChange)
-            }
+        val panel = JPanel(BorderLayout())
+        panel.add(createActionsPanel(predictionTreeChange), BorderLayout.PAGE_START)
+        panel.add(createTreeScrollPane(predictionTreeChange), BorderLayout.CENTER)
+
+        return panel
+    }
+
+    private fun createForgottenFilesString() = buildString {
+        val modifiedFilesCount = files.filterIsInstance<PredictedChange>()
+                .filter { it.change.type == Type.MODIFICATION }
+                .size
+        val unmodifiedFilesCount = files.size - modifiedFilesCount
+        if (modifiedFilesCount > 0) {
+            append("commit ")
+        }
+        if (modifiedFilesCount > 0 && unmodifiedFilesCount > 0) {
+            append("or ")
+        }
+        if (unmodifiedFilesCount > 0) {
+            append("modify ")
+        }
+        if (files.size > 1) {
+            append("these files")
+        } else {
+            append("this file")
         }
     }
 
     override fun createCenterPanel() = panel {
         row {
-            cell(true) {
-                JBLabel("You might have forgotten to modify/commit " +
-                        "${if (files.size > 1) "these files" else "this file"}:")()
-                createTreePanel()(growX)
-            }
+            JBLabel("You might have forgotten to ${createForgottenFilesString()}:")()
+        }
+        row {
+            createTreePanel()(grow, push)
         }
     }
 
