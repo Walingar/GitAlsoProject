8fef585c24e233a30bb41d946a525276fbf56734
Walingar <Walingar@mail.ru>
2019-02-03 00:35:17 +0300

    Commit is data class now

--- src/com/jetbrains/gitalso/commit/info/Commit.kt
+++ src/com/jetbrains/gitalso/commit/info/Commit.kt
@@ -2,28 +2,4 @@ package com.jetbrains.gitalso.commit.info
 
 import com.intellij.openapi.vcs.FilePath
 
-class Commit(val id: Int, val time: Long) {
-    private val _files = HashSet<FilePath>()
-
-    val files: Set<FilePath>
-        get() = _files
-
-    fun addFile(file: FilePath) {
-        _files.add(file)
-    }
-
-    override fun toString() = id.toString()
-
-    override fun hashCode() = id
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Commit
-
-        if (id != other.id) return false
-
-        return true
-    }
-}
+data class Commit(val id: Int, val time: Long, val files: Set<FilePath>)
